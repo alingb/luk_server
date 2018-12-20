255bbb1fed79c1cc9264bedd2a55276196eb38e5
master <375272990@qq.com>
2018-12-20 15:03:14 +0800

    Default Changelist

--- static/lukMsg/scripts/lukservice.js
+++ static/lukMsg/scripts/lukservice.js
@@ -9,7 +9,7 @@ var FileTableInit = function () {
     //初始化Table
     oFileTableInit.Init = function () {
         $('#luk_service').bootstrapTable({
-            url: '/lukServer/lukService',         //请求后台的URL（*）
+            url: '/lukServer/lukServerMsg',         //请求后台的URL（*）
             method: 'get',    //请求方式（*）
             toolbar: '#toolbar',                //工具按钮用哪个容器
             striped: true,                      //是否显示行间隔色
